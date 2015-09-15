95eb3c2afe66b98d095109caa8c96ca5b1b3410f
root <root@dev01-keystone-001.os.cloud.twc.net>
2015-09-15 03:18:16 +0000

    Lots of work...

--- listen.py
+++ listen.py
@@ -21,6 +21,66 @@ rabbit_host = ""
 
 KEYSTONE_RABBIT_QUEUE='keystone.info'
 
+class CADFEvent():
+    # this code makes me sad.
+    def __init__(self, json_data):
+        self.json_data = json_data
+#        print "event_type: %s" % json_data['event_type']
+        self.event_type = json_data['event_type']
+        self.timestamp = json_data['timestamp']
+        self.outcome = json_data['payload']['outcome']
+        try:
+            initiator = json_data['payload']['initiator']
+            self.initiator_host = initiator['host']['address']
+            self.initiator_host_agent = initiator['host']['agent']
+            self.initiator_user_id = initiator['id']
+        except Exception as e:
+            self.initiator_host = "unknown"
+            self.initiator_host_agent = "unknown"
+            self.initiator_user_id = "unknown"
+        try:
+            self.initiator_project_id = initiator['project_id']
+        except Exception as e:
+            self.initiator_project_id = "unknown"
+        try:
+            target = json_data['payload']['target']
+            self.target_id = target['id']
+            self.target_type_uri = target['typeURI']
+            self.target_type = self.target_type_uri.split('/')[-1:][0]
+        except Exception as e:
+            self.target_type_uri = "unknown"
+            self.target_type = "unknown"
+
+    def get_target_name(self):
+        if 'user' in self.target_type:
+            return self.get_user_name(self.target_id)
+        elif 'project' in self.target_type:
+            return self.get_project_name(self.target_id)
+        else:
+            return "unknown target type %s (id:%s)" % (self.target_type, self.target_id)
+
+    def get_initiator_project_name(self):
+        return self.get_project_name(self.initiator_project_id)
+
+    def get_initiator_user_name(self):
+        return self.get_user_name(self.initiator_user_id)
+
+    def get_user_name(self, id):
+        try:
+            return users_hash[id].name
+        except Exception:
+            return "unknown (%s)" % id
+
+    def get_project_name(self, id):
+        try:
+            return projects_hash[id].name
+        except Exception:
+            return "unknown (%s)" % id
+
+    def __str__(self):
+        return json.dumps(self.json_data, sort_keys=True, indent=4, separators=(',', ': '))
+
+
 def build_project_dict(keystone):
     global projects_hash
     projects = keystone.projects.list()
@@ -58,28 +118,36 @@ def setup_connection():
     return channel
 
 
-def callback(ch, method, properties, body):
-    try:
-        event = json.loads(body)
-        if event['event_type'] == 'identity.authenticate':
-           next
-        elif event['event_type'] == 'identity.user.created':
-            # need to rebuild our user list for this event, our
-            # keystone context could be old (expired token), so
-            # get a new one.
-            keystone = get_keystone_context()
-            build_user_dict(keystone)
-            print "NEW USER CREATED: %r" % (body,)
-        elif event['event_type'] == 'identity.user.deleted':
-            deleted_user_id = event['payload']['resource_info']
-            try:
-                print "Deleted: "
-                print(users_hash[deleted_user_id])
-            except Exception as e:
-                print e
+def get_crud_message(event):
+    return "%s %s at %s by %s (project: %s)" % \
+        (event.target_type, event.get_target_name(), 
+        event.timestamp, 
+        event.get_initiator_user_name(),
+        event.get_initiator_project_name())
 
-    except:
-        print "Invalid or unexpected format"
+
+def callback(ch, method, properties, body):
+    event = CADFEvent(json.loads(body))
+    if event.event_type == 'identity.authenticate':
+       next
+    elif event.event_type == 'identity.user.created':
+        # need to rebuild our user list for this event, our
+        # keystone context could be old (expired token), so
+        # get a new one.
+        keystone = get_keystone_context()
+        build_user_dict(keystone)
+        print "USER CREATED: %s" % get_crud_message(event)
+    elif event.event_type == 'identity.user.deleted':
+        print "USER DELETED: %s" % get_crud_message(event)
+    elif event.event_type == 'identity.project.created':
+        # need to rebuild our project list for this event, our
+        # keystone context could be old (expired token), so
+        # get a new one.
+        keystone = get_keystone_context()
+        build_project_dict(keystone)
+        print "PROJECT CREATED: %s" % get_crud_message(event)
+    elif event.event_type == 'identity.project.deleted':
+        print "PROJECT DELETED: %s" % get_crud_message(event)
 
 
 def main():
